3bc026080998a4c7292de80dc978d2221c7ef2c2
sheldontan <sheldontan@gmail.com>
2019-11-16 13:50:07 -0600

    Initial move to api

--- app.js
+++ app.js
@@ -11,54 +11,18 @@ client.on('message', message => {
   let content = message.content;
   let command = content.split(" ")[0];
 
-  if(command !== '/mock') { return; }
+  if(!command.startsWith("/")) { return; }
   let rawCaption = content.slice(command.length + 1, content.length);
 
-  console.log(rawCaption);
-    if (rawCaption.length === 0) {
-        message.reply('Usage: /mock <my caption>');
-    } else {
-      transformText(rawCaption, (transformed) => {
-          generateImage(transformed, (url) => {
-              message.reply(url);
-          });
-      });
-    }
+  if (rawCaption.length === 0) { return; }
+  processCommand(command, '', rawCaption, (response) => {
+    message.reply(response)
+  });
 });
-
 client.login(process.env.TOKEN);
 
-function generateImage(caption, callback) {
-  request.post('https://api.imgflip.com/caption_image',
-    { form:
-      { template_id: '102156234',
-        username: process.env.IMGFLIP_UN,
-        password: process.env.IMGFLIP_PW,
-          boxes: [{
-              text: caption,
-              x: 10,
-              y: 10,
-              width: 475,
-              height: 100,
-              color: "#ffffff",
-              outline_color: "#000000"
-          }]
-      }
-    }, (error, httpResponse, body) => {
-    let response = JSON.parse(body);
-    return callback(response.data.page_url);
+function processCommand(command, templateid, caption, callback) {
+  request.post(process.env.caption_url, {json: {'command': command, 'templateid': templateid, 'caption': caption}, headers: {'x-api-key': process.env.api_key}}, (error, httpResponse, body) => {
+    return callback(JSON.parse(response))
   });
-}
-
-function transformText(rawCaption, callback) {
-  let transformed = '';
-  for(let i = 0; i < rawCaption.length; i++) {
-    if(i % 2 === 0) {
-      transformed += rawCaption.charAt(i).toLowerCase();
-    } else {
-        transformed += rawCaption.charAt(i).toUpperCase();
-    }
-  }
-
-  return callback(transformed);
 }
